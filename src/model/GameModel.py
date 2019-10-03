13830ea01ea79a3399ab56236cca007c17a485d4
wizi4d <wizi4d@gmail.com>
2019-10-03 15:36:30 +0300

    feat: базовая реализация поворота налево

--- src/model/GameModel.py
+++ src/model/GameModel.py
@@ -14,6 +14,16 @@ class Snake:
         self.body = [Point2D(5, 5), Point2D(5, 6)]
         self.direction = Directions.Up
 
+    def turn_left(self):
+        if self.direction == Directions.Up:
+            self.direction = Directions.Left
+        elif self.direction == Directions.Left:
+            self.direction = Directions.Down
+        elif self.direction == Directions.Down:
+            self.direction = Directions.Right
+        elif self.direction == Directions.Right:
+            self.direction = Directions.Up
+
 
 class Point2D():
     def __init__(self, x, y):
