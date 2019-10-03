dd02af9c3ee0f1985af13772a6234fa82ea9659e
wizi4d <wizi4d@gmail.com>
2019-10-03 15:04:43 +0300

    refactor: setup method

--- tests/test_snake.py
+++ tests/test_snake.py
@@ -1,18 +1,22 @@
-from src.model.GameModel import GameWorld, Snake, Directions
+from src.model.GameModel import GameWorld, Directions
 
 
 class TestSnake:
+    def setup_method(self):
+        self.game_world = GameWorld()
+
     def test_should_init_game_world(self):
-        game_world = GameWorld()
-        assert game_world.height == 10
-        assert game_world.width == 10
+        assert self.game_world.height == 10
+        assert self.game_world.width == 10
 
-        assert game_world.snake != None, "snake exists"
-        assert len(game_world.snake.body) == 2
-        assert game_world.snake.direction == Directions.Up
-        assert game_world.snake.body[0].x == 5
-        assert game_world.snake.body[0].y == 5
+        snake = self.game_world.snake
+        assert snake is not None, "snake exists"
+        assert len(snake.body) == 2
+        assert snake.direction == Directions.Up
+        assert snake.body[0].x == 5
+        assert snake.body[0].y == 5
 
-        assert game_world.apple != None, "apple exists"
-        assert game_world.apple.x >= 0 and game_world.apple.x < 10
-        assert game_world.apple.y >= 0 and game_world.apple.y < 10
+        apple = self.game_world.apple
+        assert apple is not None, "apple exists"
+        assert 0 <= apple.x < 10
+        assert 0 <= apple.y < 10
